2a9f5b21d90fd0d35aa9dac69fba0fb61743f498
Michael Kushnir <125713208+mkush12@users.noreply.github.com>
2023-03-21 15:59:35 -0400

    mysh.c

--- mysh.c
+++ mysh.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 
-
+typedef struct input_struct{
+    char[] cmd
+    *char[] args
+}input;
 
 
 
@@ -20,5 +24,12 @@
 
 int main(int argc, char** argv){
 
-    
+    if (argc > 1){
+        //interactive mode
+
+    }
+    else{
+        //bash mode
+
+    }
 }
