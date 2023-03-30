60fc1081882b09502960e9951e49fedd8602b763
Michael Kushnir <125713208+mkush12@users.noreply.github.com>
2023-03-29 21:50:07 -0400

    test file

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
+#set up test environment as well as test basic creation and navigation shell commands
 touch foo bar baz
 mkdir test
 echo "this is the text for foo" > foo
@@ -12,28 +13,42 @@ echo "This is the text in new.txt" > new.txt
 ls
 pwd
 cd ..
+
+#test display and redirect commands
 cat foo
 cat foo bar baz
 cat < input.txt 
 echo "This is a test for output redirection." > output.txt
 cat < foo > output2.txt
 touch file1.txt file2.txt other.txt
+
+#test basic piping
 ls | grep file
+ls | cat
+
+
+#more intense redirect tests
 echo foo bar > baz
 echo foo > baz bar
 echo > baz foo bar
 sort < foo > bar
 sort > bar < foo
-echo foo > bar | cat
 echo foo > bar > baz
 cat < foo < bar
+
+
+#testing the interaction between redirect and pipes. Shouldn't do anything unreasonable
+echo foo > bar | cat
 echo foo | cat < bar
 pwd > output.txt
 touch foo | cd some_dir
 cd some_dir | pwd
+
+#testing file wildcards and directory wildcards
 ls test/*.c
 echo */foo
 
+#test compilation and permission modification
 gcc testing.c -o testing
 ./testing
 ./non
@@ -42,7 +57,12 @@ chmod -x testing
 chmod +x testing
 ./testing
 
-cat script.txt | grep me | unique
+#testing home directory
+ls ~
+echo ~
+
+
+echo 
 
 touch foo | exit
 exit | echo bar
