93c472bd6e9fc86b7502d8b107236aff35a3a5c2
Michael Kushnir <125713208+mkush12@users.noreply.github.com>
2023-03-29 22:25:33 -0400

    test file

--- test.sh
+++ test.sh
@@ -47,6 +47,8 @@ cd test | pwd
 #testing file wildcards and directory wildcards
 ls test/*.c
 echo */foo
+cat */foo
+echo */*.c
 
 #test compilation and permission modification
 gcc testing.c -o testing
